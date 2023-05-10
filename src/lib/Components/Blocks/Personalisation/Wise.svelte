8882834e130f05dd984c82db686e4b9945c13b52
Andry Pedak <andry.pedak@gmail.com>
2023-05-10 15:41:50 +0000

    Improve Wise

--- src/lib/Components/Blocks/Personalisation/Wise.svelte
+++ src/lib/Components/Blocks/Personalisation/Wise.svelte
@@ -1,5 +1,5 @@
 <div class="relative">
-  <div class="absolute -z-10 inset-0 -bottom-4 shadow rounded-[2rem] bg-[url('$lib/assets/wise-bg.svg')]" />
+  <div class="absolute -z-10 inset-0 -bottom-4 shadow rounded-[2rem] bg-[url('$lib/assets/wise-bg.svg')] bg-bottom bg-cover bg-no-repeat" />
 
   <div class="rounded-[2rem] rounded-tl-xl rounded-tr-xl p-8 bg-white dark:bg-gray-950 dark:shadow-2xl">
     <h2 class="text-lg font-semibold mb-8">Hello Wise 👋🏼</h2>

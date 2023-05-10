7493d0e0384fff170c86f03ed9452577b8b2c759
aanndryyyy <andry.pedak@gmail.com>
2023-05-10 17:58:01 +0000

    Deploying to gh-pages from @ aanndryyyy/resume@93afdae9a1ad22ea75824a41f381401b9f3dff6c 🚀

---
+++ _app/immutable/chunks/stores.3b1c46c3.js
@@ -0,0 +1,1 @@
+import"./index.60b7fe90.js";import{s as e}from"./singletons.6eac0782.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};

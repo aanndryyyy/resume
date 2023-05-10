eba7d04556f036dd7533b0f6fbe74e19de136675
Andry Pedak <andry.pedak@gmail.com>
2023-05-10 14:13:19 +0000

    Improve personalisation

--- src/lib/Components/Blocks/Company/Wise.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Card from '$lib/Components/Card.svelte';
-</script>
-
-<Card>
-  <h2 class="text-lg font-semibold mb-8">Hello Wise 👋🏼</h2>
-
-  <p class="text-gray-600 dark:text-gray-400">Libero quas veritatis nulla distinctio fuga nihil temporibus et. Quia dicta sapiente qui porro molestiae nobis incidunt voluptatem. Et voluptas sunt nihil. At perferendis voluptatem dolores nulla. Adipisci dolore non. Praesentium ipsa magnam ut quia explicabo voluptates.</p>
-</Card>
---
+++ src/lib/Components/Blocks/Personalisation/Wise.svelte
@@ -0,0 +1,8 @@
+<div class="relative">
+  <div class="absolute -z-10 inset-0 -bottom-4 shadow rounded-[2rem] bg-[url('$lib/assets/wise-bg.svg')]" />
+
+  <div class="rounded-[2rem] rounded-tl-xl rounded-tr-xl p-8 bg-white dark:bg-gray-950 dark:shadow-2xl">
+    <h2 class="text-lg font-semibold mb-8">Hello Wise 👋🏼</h2>
+    <p class="text-gray-600 dark:text-gray-400">Libero quas veritatis nulla distinctio fuga nihil temporibus et. Quia dicta sapiente qui porro molestiae nobis incidunt voluptatem. Et voluptas sunt nihil. At perferendis voluptatem dolores nulla. Adipisci dolore non. Praesentium ipsa magnam ut quia explicabo voluptates.</p>
+  </div>
+</div>

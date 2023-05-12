8dedd7421c1b6bbd10521061697a95c47fc8fdf2
Andry Pedak <andry.pedak@gmail.com>
2023-05-12 16:20:53 +0000

    Refactoring

---
+++ src/lib/Components/Blocks/Experience/New/NewExperience.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title: string;
+	export let subtitle: string;
+</script>
+
+<article class="border-t border-gray-200 p-6 dark:border-gray-700 md:p-8">
+	<header class="space-y-4 md:space-y-6">
+		<div class="flex justify-between">
+			<h3 class="font-medium">{title}</h3>
+			<p class="text-sm text-gray-600 dark:text-gray-400">{subtitle}</p>
+		</div>
+
+		<div class="flex gap-4 overflow-x-scroll">
+			<slot />
+		</div>
+	</header>
+</article>
--- src/lib/Components/Blocks/Experience/New/NewExperienceItems.svelte
+++ src/lib/Components/Blocks/Experience/New/NewExperienceItems.svelte
@@ -6,121 +6,84 @@
 	import taltech from '$lib/assets/taltech.png';
 
 	import ExperienceItem from './NewExperienceItem.svelte';
+	import NewExperience from './NewExperience.svelte';
 </script>
 
-<article class="border-t border-gray-200 p-6 dark:border-gray-700 md:p-8">
-	<header class="space-y-4 md:space-y-6">
-		<div class="flex justify-between">
-			<h3 class="font-medium">Management & Team Leadership</h3>
-			<p class="text-sm text-gray-600 dark:text-gray-400">2 yrs 3 mos</p>
-		</div>
+<NewExperience title="Management & Team Leadership" subtitle="2 yrs 3 mos">
+	<ExperienceItem title="IT Service Lead" subtitle="Stora Enso &middot; 1 yrs 5 mos" current={true}>
+		<img
+			slot="logo"
+			src={storaenso}
+			alt="Stora Enso"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
 
-		<div class="flex gap-4 overflow-x-scroll">
-			<ExperienceItem
-				title="IT Service Lead"
-				subtitle="Stora Enso &middot; 1 yrs 5 mos"
-				current={true}
-			>
-				<img
-					slot="logo"
-					src={storaenso}
-					alt="Stora Enso"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
+	<ExperienceItem title="Experienced Service Specialist" subtitle="Stora Enso &middot; 10 mos">
+		<img
+			slot="logo"
+			src={storaenso}
+			alt="Stora Enso"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
 
-			<ExperienceItem title="Experienced Service Specialist" subtitle="Stora Enso &middot; 10 mos">
-				<img
-					slot="logo"
-					src={storaenso}
-					alt="Stora Enso"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
+	<ExperienceItem title="Management" subtitle="MSSc &middot; Tallinn University of Technology">
+		<img
+			slot="logo"
+			src={taltech}
+			alt="TalTech"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
+</NewExperience>
 
-			<ExperienceItem title="Management" subtitle="MSSc &middot; Tallinn University of Technology">
-				<img
-					slot="logo"
-					src={taltech}
-					alt="TalTech"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
-		</div>
-	</header>
-</article>
+<NewExperience title="Development & DevOps" subtitle="6 yrs 2 mos">
+	<ExperienceItem title="Full-Stack Web Developer" subtitle="OK Interactive (OKIA) &middot; 1 yr">
+		<OKIAIcon slot="logo" />
+	</ExperienceItem>
 
-<article class="border-t border-gray-200 p-6 dark:border-gray-700 md:p-8">
-	<header class="space-y-4 md:space-y-6">
-		<div class="flex justify-between">
-			<h3 class="font-medium">Development & DevOps</h3>
-			<p class="text-sm text-gray-600 dark:text-gray-400">6 yrs 2 mos</p>
-		</div>
+	<ExperienceItem title="Web Developer" subtitle="Web Systems &middot; 8 mos">
+		<WebSystemsIcon slot="logo" />
+	</ExperienceItem>
 
-		<div class="flex gap-4 overflow-x-scroll">
-			<ExperienceItem
-				title="Full-Stack Web Developer"
-				subtitle="OK Interactive (OKIA) &middot; 1 yr"
-			>
-				<OKIAIcon slot="logo" />
-			</ExperienceItem>
+	<ExperienceItem
+		title="Computer Systems & Engineering"
+		subtitle="BSc(Eng) &middot; Tallinn University of Technology"
+	>
+		<img
+			slot="logo"
+			src={taltech}
+			alt="TalTech"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
 
-			<ExperienceItem title="Web Developer" subtitle="Web Systems &middot; 8 mos">
-				<WebSystemsIcon slot="logo" />
-			</ExperienceItem>
+	<ExperienceItem title="Developer" subtitle="Freelance &middot; 4 yrs 4 mos" />
+</NewExperience>
 
-			<ExperienceItem
-				title="Computer Systems & Engineering"
-				subtitle="BSc(Eng) &middot; Tallinn University of Technology"
-			>
-				<img
-					slot="logo"
-					src={taltech}
-					alt="TalTech"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
+<NewExperience title="Digital Product & Service Management" subtitle="5 yrs 9 mos">
+	<ExperienceItem title="IT Service Lead" subtitle="Stora Enso &middot; 1 yrs 5 mos" current={true}>
+		<img
+			slot="logo"
+			src={storaenso}
+			alt="Stora Enso"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
 
-			<ExperienceItem title="Developer" subtitle="Freelance &middot; 4 yrs 4 mos" />
-		</div>
-	</header>
-</article>
+	<ExperienceItem title="COO & Developer" subtitle="UusKorter &middot; 3 yrs 3 mos">
+		<UusKorterIcon slot="logo" />
+	</ExperienceItem>
 
-<article class="border-t border-gray-200 p-6 dark:border-gray-700 md:p-8">
-	<header class="space-y-4 md:space-y-6">
-		<div class="mb-4 flex justify-between">
-			<h3 class="font-medium">Digital Product & Service Management</h3>
-			<p class="text-sm text-gray-600 dark:text-gray-400">5 yrs 9 mos</p>
-		</div>
+	<ExperienceItem title="Developer" subtitle="Freelance &middot; 4 yrs 4 mos" />
 
-		<div class="flex gap-4 overflow-x-scroll">
-			<ExperienceItem
-				title="IT Service Lead"
-				subtitle="Stora Enso &middot; 1 yrs 5 mos"
-				current={true}
-			>
-				<img
-					slot="logo"
-					src={storaenso}
-					alt="Stora Enso"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
-
-			<ExperienceItem title="COO & Developer" subtitle="UusKorter &middot; 3 yrs 3 mos">
-				<UusKorterIcon slot="logo" />
-			</ExperienceItem>
-
-			<ExperienceItem title="Developer" subtitle="Freelance &middot; 4 yrs 4 mos" />
-
-			<ExperienceItem title="Management" subtitle="MSSc &middot; Tallinn University of Technology">
-				<img
-					slot="logo"
-					src={taltech}
-					alt="TalTech"
-					class="h-full w-full object-contain object-center"
-				/>
-			</ExperienceItem>
-		</div>
-	</header>
-</article>
+	<ExperienceItem title="Management" subtitle="MSSc &middot; Tallinn University of Technology">
+		<img
+			slot="logo"
+			src={taltech}
+			alt="TalTech"
+			class="h-full w-full object-contain object-center"
+		/>
+	</ExperienceItem>
+</NewExperience>

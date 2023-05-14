b10e5600687799c3421887cea6d4ceafa41ef41c
Andry Pedak <andry.pedak@gmail.com>
2023-05-14 17:24:31 +0000

    Open project in new tab

--- src/lib/Components/Blocks/Projects.svelte
+++ src/lib/Components/Blocks/Projects.svelte
@@ -8,6 +8,8 @@
 	<div class="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-8">
 		<a
 			class="group block space-y-4 rounded bg-white px-4 py-3 ring-2 ring-blue-900/10 hover:bg-blue-600 hover:ring-blue-600 dark:bg-gray-950 dark:ring-gray-800 dark:hover:bg-blue-700 dark:hover:ring-blue-700"
+			target="_blank"
+			rel="noopener noreferrer"
 			href="https://uuskorter.ee"
 		>
 			<h3 class="font-semibold group-hover:text-white">
@@ -24,6 +26,8 @@
 
 		<a
 			class="group block space-y-4 rounded bg-white px-4 py-3 ring-2 ring-blue-900/10 hover:bg-blue-600 hover:ring-blue-600 dark:bg-gray-950 dark:ring-gray-800 dark:hover:bg-blue-700 dark:hover:ring-blue-700"
+			target="_blank"
+			rel="noopener noreferrer"
 			href="https://aanndryyyy.github.io/car-sharing-comparison/"
 		>
 			<h3 class="font-semibold group-hover:text-white">

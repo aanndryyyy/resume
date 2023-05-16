879b053a6461cda165daa15247331dddd123dab4
Andry Pedak <andry.pedak@gmail.com>
2023-05-16 18:10:03 +0000

    Refactor

--- src/lib/Components/Card.svelte
+++ src/lib/Components/Card.svelte
@@ -1,7 +1,10 @@
-<aside
-	class={`overflow-hidden rounded-xl bg-white bg-cover bg-center bg-no-repeat p-6 shadow dark:bg-gray-950 dark:bg-none dark:shadow-2xl md:p-8 
-	${$$props.class || ''}`}
-	id={$$props.id || ''}
->
+<aside class={`card ${$$props.class || ''}`} id={$$props.id || ''}>
 	<slot />
 </aside>
+
+<style lang="postcss">
+	.card {
+		@apply overflow-hidden rounded-xl bg-white bg-cover bg-center bg-no-repeat p-6 shadow md:p-8;
+		@apply dark:bg-gray-950 dark:bg-none dark:shadow-2xl;
+	}
+</style>
